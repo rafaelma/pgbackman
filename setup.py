67eed164403c1fd9a5d74856f299cb49bb3c351e
Rafael Martinez Guerrero <rafael@postgresql.org.es>
2014-06-22 12:28:10 +0200

    Use the right init.d script when installing manually via source and setup.py

--- setup.py
+++ setup.py
@@ -20,6 +20,8 @@
 # along with Pgbackman.  If not, see <http://www.gnu.org/licenses/>.
 
 import subprocess
+import platform
+import shutil
 import sys
 import os
 import pwd
@@ -68,15 +70,34 @@ try:
     else:
         raise SystemExit('ERROR: Problems creating user pgbackman. Returncode: ' + str(proc.returncode))
     
+    #
+    # Check linux distribution and define init script
+    #
+
+    distro = platform.linux_distribution()[0]
+
+    if distro == 'CentOS' or distro == 'Red Hat Enterprise Linux Server' or distro == 'Red Hat Enterprise Linux Workstation':
+        init_file = 'etc/pgbackman_init_rh.sh'
+        shutil.copy2(init_file, '/tmp/pgbackman')
+
+    
+    elif distro == 'debian' or distro == 'Ubuntu':
+        init_file = 'etc/pgbackman_init_debian.sh'
+        shutil.copy2(init_file, '/tmp/pgbackman')
+        
+    #
+    # Setup
+    #
+
     setup(name='pgbackman',
           version=pgbackman['__version__'],
           description='PGBACKMAN - PostgreSQL Backup Manager',
           author='Rafael Martinez Guerrero',
           author_email='rafael@postgresql.org.es',
-          url='https://github.com/rafaelma/pgbackman',
+          url='http://www.pgbackman.org/',
           packages=['pgbackman',],
           scripts=['bin/pgbackman','bin/pgbackman_control','bin/pgbackman_maintenance','bin/pgbackman_dump','bin/pgbackman_restore'],
-          data_files=[('/etc/init.d', ['bin/pgbackman.sh']),
+          data_files=[('/etc/init.d', ['/tmp/pgbackman']),
                       ('/etc/pgbackman', ['etc/pgbackman.conf']),
                       ('/var/log/pgbackman',['etc/pgbackman.log'])],
           install_requires=install_requires,
