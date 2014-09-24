0e4330eba99c12ae5ea25cb77e302f23bcc19c2a
Rafael Martinez Guerrero <rafael@postgresql.org.es>
2014-09-24 09:13:18 +0200

    Fix show_snapshots_in_progress output

--- sql/pgbackman_2.sql
+++ sql/pgbackman_2.sql
@@ -13,22 +13,22 @@
 BEGIN;
 
 CREATE OR REPLACE VIEW show_snapshots_in_progress AS
-SELECT lpad(a.snapshot_id::text, 11, '0'::text) AS "SnapshotID",
-       date_trunc('seconds'::text, a.registered) AS "Registered",
-       a.backup_server_id,
-       get_backup_server_fqdn(a.backup_server_id) AS "Backup server",
-       a.pgsql_node_id,
-       get_pgsql_node_fqdn(a.pgsql_node_id) AS "PgSQL node",
-       a.dbname AS "DBname",
-       to_char(a.at_time, 'YYYYMMDDHH24MI'::text) AS "AT time",
-       a.backup_code AS "Code",
-       a.retention_period::text AS "Retention",
-       'IN PROGRESS'::text AS "Status"
+   SELECT lpad(a.snapshot_id::text, 11, '0'::text) AS "SnapshotID",
+   	  date_trunc('seconds'::text, a.registered) AS "Registered",
+       	  a.backup_server_id,
+       	  get_backup_server_fqdn(a.backup_server_id) AS "Backup server",
+       	  a.pgsql_node_id,
+       	  get_pgsql_node_fqdn(a.pgsql_node_id) AS "PgSQL node",
+       	  a.dbname AS "DBname",
+       	  to_char(a.at_time, 'YYYY-MM-DD HH24:MI:SS'::text) AS "AT time",
+       	  a.backup_code AS "Code",
+       	  date_trunc('second',now()-a.at_time)::text AS "Elapsed time",
+       	  'IN PROGRESS'::text AS "Status"
    FROM snapshot_definition a
    LEFT JOIN backup_catalog b
    ON a.snapshot_id = b.snapshot_id 
    WHERE b.snapshot_id IS NULL
-   ORDER BY to_char(a.at_time, 'YYYYMMDDHH24MI'::text);
+   ORDER BY a.at_time ASC;
 
 ALTER VIEW show_snapshots_in_progress OWNER TO pgbackman_role_rw;
 
