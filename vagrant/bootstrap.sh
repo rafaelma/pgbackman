de834c93c638c701bd4c85c76438c6d401fc1ec0
rafaelma <rafael@postgresql.org.es>
2015-09-28 13:15:10 +0200

    Add pg-node02 to Vagranr config

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -13,4 +13,5 @@ echo "10.10.10.101   pg-backup01.example.net   pg-backup01" >> /etc/hosts
 echo "10.10.10.102   pg-backup02.example.net   pg-backup02" >> /etc/hosts
 echo "10.10.10.103   pg-node01.example.net   pg-node01" >> /etc/hosts
 echo "10.10.10.104   pgbackmandb.example.net   pgbackmandb" >> /etc/hosts
+echo "10.10.10.105   pg-node02.example.net   pg-node02" >> /etc/hosts
 
